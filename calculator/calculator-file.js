b2ee8c4d6bdd0be6809d739fd290407b3ef185f5
sebcroker <seb.h.croker@gmail.com>
2024-12-10 21:06:16 +1100

    Added files from local host to remote repository

---
+++ calculator/calculator-file.js
@@ -0,0 +1,16 @@
+function addition(a, b) { // ...args changes all the inputs into an array of elements
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
